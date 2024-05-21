49f1fca1fd6802f7c7380800bedb9a831d3faaed
Tornike Rikiashvili <tornikerikiashvili@gmail.com>
2024-05-21 17:39:54 +0400

    final corrections vol 2

--- resources/views/components/sections/projects.blade.php
+++ resources/views/components/sections/projects.blade.php
@@ -35,7 +35,7 @@
                     <div data-animation-container class="six-columns">
                         <div class="content-right-margin-40">
                             <span class="small-title-oswald red-color overlay-anim-box2" data-animation="overlay-anim2">{{data_get($project, 'type')}}</span>
-                            <h3 class="title-style text-color-1">
+                            <h3 class="project_item_title title-style text-color-1">
                                 <span data-animation-child class="overlay-anim-box2 light-bg-1 tr-delay01" data-animation="overlay-anim2">{{data_get($project, 'title')}}</span>
                             </h3>
                             <p data-animation-child class="p-style-small text-color-2 fade-anim-box tr-delay04" data-animation="fade-anim">{{data_get($project, 'teaser')}}</p>
@@ -75,7 +75,7 @@
                     <div data-animation-container class="six-columns">
                         <div class="content-left-margin-40">
                             <span class="small-title-oswald red-color overlay-anim-box2" data-animation="overlay-anim2">{{data_get($project, 'type')}}</span>
-                            <h3 class="title-style text-color-1">
+                            <h3 class="project_item_title title-style text-color-1">
                                 <span data-animation-child class="overlay-anim-box2 light-bg-1 tr-delay01" data-animation="overlay-anim2">{{data_get($project, 'title')}}</span>
                             </h3>
                             <p data-animation-child class="p-style-small text-color-2 fade-anim-box tr-delay04" data-animation="fade-anim">{{data_get($project, 'teaser')}}</p>
@@ -103,7 +103,7 @@
                     <div data-animation-container class="six-columns">
                         <div class="content-right-margin-40">
                             <span class="small-title-oswald red-color overlay-anim-box2" data-animation="overlay-anim2">{{data_get($project, 'type')}}</span>
-                            <h3 class="title-style text-color-1">
+                            <h3 class="project_item_title title-style text-color-1">
                                 <span data-animation-child class="overlay-anim-box2 light-bg-1 tr-delay01" data-animation="overlay-anim2">{{data_get($project, 'title')}}</span>
                             </h3>
                             <p data-animation-child class="p-style-small text-color-2 fade-anim-box tr-delay04" data-animation="fade-anim">{{data_get($project, 'teaser')}}</p>
@@ -144,9 +144,7 @@
                         <div class="content-left-margin-40">
                             <span class="small-title-oswald red-color overlay-anim-box2" data-animation="overlay-anim2">{{data_get($project, 'type')}}</span>
                             <h3 class="project_item_title title-style text-color-1">
-                                <span data-animation-child class="overlay-anim-box2 light-bg-1 tr-delay01" data-animation="overlay-anim2">{{data_get($project, 'title.one')}}</span><br>
-                                <span data-animation-child class="overlay-anim-box2 light-bg-1 tr-delay02" data-animation="overlay-anim2">{{data_get($project, 'title.two')}}</span><br>
-                                <span data-animation-child class="overlay-anim-box2 light-bg-1 tr-delay03" data-animation="overlay-anim2">{{data_get($project, 'title.three')}}</span>
+                                <span data-animation-child class="overlay-anim-box2 light-bg-1 tr-delay01" data-animation="overlay-anim2">{{data_get($project, 'title')}}</span>
                             </h3>
                             <p data-animation-child class="p-style-small text-color-2 fade-anim-box tr-delay04" data-animation="fade-anim">{{data_get($project, 'teaser')}}</p>
 
