eb1301980b06b638fc55371392632cb7fc3c2e2d
Tornike Rikiashvili <tornikerikiashvili@gmail.com>
2024-06-05 14:41:49 +0400

    lazyloading of products

--- resources/views/livewire/search-form.blade.php
+++ resources/views/livewire/search-form.blade.php
@@ -9,7 +9,7 @@
                <li class="result_box">
                     <a href="{{App::getLocale() . '/product/' . data_get($item, 'id') }}">
                         <div class="result_box_img">
-                            <img src="{{data_get($item, 'cover_image.url')}}">
+                            <img loading="lazy" src="{{data_get($item, 'cover_image.url')}}">
                         </div>
                         <div class="result_title">
                             <span>{{data_get($item, 'name')}}</span>
