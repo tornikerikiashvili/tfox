7ae54262dfe5c3832bc915dd49a1e8d04642517a
Tornike Rikiashvili <tornikerikiashvili@gmail.com>
2024-06-14 02:44:28 +0400

    fix unique slug

--- app/Filament/Resources/Ecommerce/ProductsResource.php
+++ app/Filament/Resources/Ecommerce/ProductsResource.php
@@ -77,7 +77,8 @@ class ProductsResource extends SimpleResource
     public static function metaFields(Card $card): Card
     {
         return $card->schema([
-            Forms\Components\TextInput::make('metadata.slug')->unique(),
+            Forms\Components\TextInput::make('metadata.slug')->unique(column: 'metadata->slug', ignoreRecord: true)
+            ->required(),
             Forms\Components\Checkbox::make('metadata.is_published')->label('Published'),
             Forms\Components\Select::make('category_id')
             ->relationship('category', 'title'),
